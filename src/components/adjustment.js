56d4cfb78e2772f2aaec64c2ae5aa2f64e841f61
mazenongithub <mazen@egeotechnical.com>
2021-10-31 15:12:31 -0700

    removed touch event

--- src/components/adjustment.js
+++ src/components/adjustment.js
@@ -310,11 +310,8 @@ class Adjustment {
                     <div style={{ ...styles.generalContainer, ...styles.bottomMargin15 }}>
                         <button
                             style={{ ...styles.generalButton, ...styles.generalLink, ...styles.headerStyle, ...styles.boldFont, ...menufont, ...styles.menuColor, ...styles.menuBackColor, ...styles.addBorderRadius5, ...styles.generalPadding, ...styles.whiteOutline, ...styles.addMargin, ...buttonBackground(),...styles.adjustButtonSize }}
-                            onTouchStart={() => { adjustment.highlightButton.call(this) }}
-                            onTouchEnd={() => { adjustment.unhighlightButton.call(this) }}
-                            onMouseDown={() => { adjustment.highlightButton.call(this) }}
-                            onMouseUp={() => { adjustment.unhighlightButton.call(this) }}
-                            onMouseLeave={() => { adjustment.unhighlightButton.call(this) }}
+                          
+                        
                             onClick={() => { adjustment.adjustEarnings.call(this) }}
                         >
 
